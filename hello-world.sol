c02db09a72edc03b6d716543017e3c2439ce660d
Noble Osinachi <nobleosinachi98@gmail.com>
2023-04-11 14:00:51 +0100

    Last updated on Tuesday April 11 2023 at 1:58:14PM
    Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
    Co-authored-by: David Popoola <davieedpopoola@gmail.com>
    Co-authored-by: Golden Chukwukere <66950981+iamGolden@users.noreply.github.com>
    Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
    Co-authored-by: Isaac Bassey <chartobyson2025@gmail.com>
    Co-authored-by: Jato Joseph <jaotjoseph4@gmail.com>
    Co-authored-by: Joan <sylvesterjoannaunyii@gmail.com>
    Co-authored-by: Joshua-Chris <joshuadgreat04@gmail.com>
    Co-authored-by: Noble Osinachi <150407018@live.unilag.edu.ng>
    Co-authored-by: PraiseGeek <praisegeek@gmail.com>
    Co-authored-by: Precious Adedibu <adedibuprecious@gmail.com>
    Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>
    Co-authored-by: iAmGolden <chuksgold29@gmail.com>
    Co-authored-by: josiahking <josiahaccounts@gmail.com>
    Co-authored-by: masaun <masaun@protonmail.com>
    Co-authored-by: femotizo <epebinuoluwafemi@gmail.com>
    Signed-off-by: Noble Osinachi <nobleosinachi98@gmail.com>

--- hello-world.sol
+++ hello-world.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 // Contract definition
 contract HelloWorld {
-  // A public function that returns a string
-  function sayHello() public pure returns (string memory) {
-    return "Hello World";
-  }
+    // A public function that returns a string
+    function sayHello() public pure returns (string memory) {
+        return "Hello World";
+    }
 }
